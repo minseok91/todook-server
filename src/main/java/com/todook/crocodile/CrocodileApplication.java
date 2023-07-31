d5b8195a94597c5781a4fc365a19c8f38bc7d716
minseok <minseok9128@gmail.com>
2023-07-31 22:09:52 +0900

    first commit

---
+++ src/main/java/com/todook/crocodile/CrocodileApplication.java
@@ -0,0 +1,13 @@
+package com.todook.crocodile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrocodileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrocodileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todook/crocodile/CrocodileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todook.crocodile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrocodileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
